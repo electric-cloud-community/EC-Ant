1e0fcf32a7552718d24d2224ad48d10db6c966ad
Valentyn Solomko <Valentyn.Solomko@portaone.com>
2019-01-02 18:09:01 +0200

    Adapt for building with Gradle

--- src/main/resources/project/manifest.pl
+++
@@ -1,6 +0,0 @@
-@files = (
-    ['//property[propertyName="ec_setup"]/value', 'ec_setup.pl'],
-    ['//procedure[procedureName="runAnt"]/step[stepName="createCommandLine"]/command','antDriver.pl'],
-    ['//procedure[procedureName="runAnt"]/propertySheet/property[propertyName="ec_parameterForm"]/value','wizardCustomEditor.xml'],
-    ['//property[propertyName="postp_matchers"]/value', 'postp_matchers.pl'],
-);
